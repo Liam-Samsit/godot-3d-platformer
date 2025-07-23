e85cc055d2ea8b81ecf80798ca3100d8ea924bb9
DESKTOP-BPI1C6P\Samuel <chebouikhaled@gmail.com>
2025-07-23 10:38:55 +0100

    Added robot model and animation. Reduced World Light.

--- steve.gd
+++ steve.gd
@@ -8,6 +8,19 @@ var xform : Transform3D
 
 func _physics_process(delta: float) -> void:
 	
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	#play robot animations
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
+		$AnimationPlayer.play("jump")
+	elif is_on_floor() && input_dir != Vector2.ZERO:
+		$AnimationPlayer.play("run")
+	elif is_on_floor() && input_dir == Vector2.ZERO:
+		$AnimationPlayer.play("idle")
+	
+	
+	
 	# rotate the camera left / right
 	if Input.is_action_just_pressed("cam_left"):
 		dir_final = dir_final + 90.0
@@ -24,15 +37,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	
 	# it's a new vector3 direction taking into account the user arrow input and camera rotation
 	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# rotate the character match so teh character rotates in relation to the camera
 	if input_dir != Vector2(0,0):
-		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) - 90
+		$Armature.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) - 90
 	
 	# rotate character to align with floor, like slopes
 	if is_on_floor():
